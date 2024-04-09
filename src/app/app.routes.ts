a75c597c060b25759042000d1da8def80b25c3e2
ahmedgebreeel <ahmedgebreeeel@gmail.com>
2024-04-09 14:32:07 +0200

    adding guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { EditProfilePersonalInfoComponent } from './components/edit-profile-personal-info/edit-profile-personal-info.component';
 import { EditProfileSocialMediaComponent } from './components/edit-profile-social-media/edit-profile-social-media.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     {path: "", redirectTo: "register", pathMatch: "full"},
     {path: "register", component:RegisterComponent },
     {path: "login", component:LoginComponent },
-    {path: "edit/personal", component:EditProfilePersonalInfoComponent },
-    {path: "edit/social", component:EditProfileSocialMediaComponent }
+    {path: "edit/personal", component:EditProfilePersonalInfoComponent, canActivate:[authGuard] },
+    {path: "edit/social", component:EditProfileSocialMediaComponent, canActivate:[authGuard] }
 ];
