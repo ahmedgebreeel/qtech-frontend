7d3673c3f6d126ac32e11a00e6b644a78ba122c7
ahmedgebreeel <ahmedgebreeeel@gmail.com>
2024-04-09 19:24:03 +0200

    adding user social links

--- src/app/components/edit-profile-social-media/edit-profile-social-media.component.ts
+++ src/app/components/edit-profile-social-media/edit-profile-social-media.component.ts
@@ -1,26 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-edit-profile-social-media',
   standalone: true,
-  imports: [FormsModule, RouterLink, RouterLinkActive],
+  imports: [FormsModule, RouterLink, RouterLinkActive, ReactiveFormsModule, HttpClientModule],
+  providers:[UserService],
   templateUrl: './edit-profile-social-media.component.html',
   styleUrl: './edit-profile-social-media.component.css'
 })
-export class EditProfileSocialMediaComponent implements OnInit {
-  currentPage: string = 'personal'; // Default to personal page
-  userInfo: any = {
-    name: '',
-    email: '',
-    twitter: '',
-    facebook: ''
-  };
+export class EditProfileSocialMediaComponent  {
+ 
+  constructor(private userService: UserService ) { }
 
-  constructor() { }
+  socialForm = new FormGroup({
+    facebook: new FormControl(''),
+    twitter: new FormControl(''),
+    linkedin: new FormControl(''),
+    instagram: new FormControl(''),
+    github: new FormControl(''),
+  });
 
-  ngOnInit(): void {
-  }
 
+  save(){
+    this.userService.editSocialMedia(this.socialForm.value).subscribe({
+      next: (respond)=>{
+        console.log(respond);
+        alert("social media link updated successfully");
+        
+      },
+      error: (err)=>{
+        console.log(err);
+        alert("error saving your social media link please try again later");
+        
+      }
+    })
+
+}
 }
